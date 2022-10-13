78c0429e67e077dd07ba5600270359e310faae32
zzemoo <chaerim.dev@gmail.com>
2022-10-13 10:52:35 +0900

    메인 데이터 시각화 작업중

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,44 +4,39 @@ const cultureData = await getCultureData('CULTURE')
 
 console.log('cultureData:',cultureData);
 
-const festival = '축제';
-const concert = '공연';
-const exhibit = '전시';
-const edu = '교육/체험';
-const el = '기타';
 
 // 분류할 데이터 객체
 const kindData = {
-    '축제' : [],
-    '공연' : [],
-    '전시' : [],
-    '교육/체험' : [],
-    '기타' : [],
+    'festival' : [],
+    'concert' : [],
+    'exhibit' : [],
+    'edu' : [],
+    'el' : [],
 };
 
 //데이터 종류별로 분류하기
 cultureData.forEach((v, i) => {
     
     if(v.CODENAME.includes('축제')) {
-        kindData['축제'].push(v);
+        kindData['festival'].push(v);
     } else if(v.CODENAME == '기타') {
-        kindData['기타'].push(v);
+        kindData['concert'].push(v);
     } else if(v.CODENAME == '문화교양/강좌') {
-        kindData['교육/체험'].push(v);
+        kindData['exhibit'].push(v);
     } else if(v.CODENAME == '전시/미술') {
-        kindData['전시'].push(v);
+        kindData['edu'].push(v);
     } else {
-        kindData['공연'].push(v);
+        kindData['el'].push(v);
     }
 })
 
-console.log(kindData['축제']);
 
 const setActiveGnb = (kind) => {
     if (kind) {
         document.querySelectorAll('.gnb a').forEach((v, i) => {
             if(v.dataset.kind == kind) {
                 v.classList.add('active');
+                console.log(v)
             } else {
                 v.classList.remove('active');
             }
@@ -50,15 +45,32 @@ const setActiveGnb = (kind) => {
 }
 
 
-
+console.log(location.search)
 const query = new URLSearchParams(location.search);
 const kind = query.get('kind');
-console.log(kind);
 setActiveGnb(kind);
 
-const currentList = kindData[kind];
+const currentList = kindData[`${kind}`];
+console.log(currentList);
 
 const container = document.querySelector('.main_container')
+
+currentList.forEach((v, i) => {
+    const item = document.createElement('div');
+    item.classList.add('item');
+
+    const a = document.createElement('a');
+    a.setAttribute('href', '#');
+
+    const img = document.createElement('img');
+    img.setAttribute('src', `${v.MAIN_IMG}`);
+    a.appendChild(img);
+
+    item.appendChild(a);
+
+    container.appendChild(item);
+    
+})
 
 
 
